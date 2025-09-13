89cffa5ef290dde80b727cddcd01988b4efecf97
Pedro <pcarrieldesouza@gmail.com>
2025-09-13 18:09:06 -0300

    Update main.py

--- main.py
+++ main.py
@@ -3,10 +3,10 @@ from fastapi import FastAPI
 app = FastAPI()
 
 
-@app.get("/")
+@app.get("/helloworld")
 async def root():
     return {"message": "Hello World"}
 
-@app.get("/teste1")
+@app.get("/funcaoteste")
 async def funcaoteste():
     return{"teste": "deu certo"}
